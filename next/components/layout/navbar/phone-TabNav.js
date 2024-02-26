da0b92eef6145f2599fbd46697404b93a18da04b
HsinYu <angela041199@gmail.com>
2024-02-26 14:10:00 +0800

    商品頁及細節頁RWD完成＋phoneNavBar公版

--- next/components/layout/navbar/phone-TabNav.js
+++ next/components/layout/navbar/phone-TabNav.js
@@ -7,44 +7,44 @@ import Link from 'next/link'
 export default function PhoneTabNav() {
   return (
     <>
-      <div className={`row g-0 text-center d-lg-none ${styles.phoneNav}`}>
+      <div class={`row g-0 text-center d-lg-none ${styles.phoneNav}`}>
         <div className="pt-1 d-flex">
-          <div className="col">
+          <div class="col">
             <Link href="" className={styles.phoneNavContent}>
               <FaHome />
               <br />
               首頁
             </Link>
           </div>
-          <div className="col">
+          <div class="col">
             <Link href="" className={styles.phoneNavContent}>
               <FaHeart />
               <br />
               追蹤清單
             </Link>
           </div>
-          <div className="col">
+          <div class="col">
             <Link href="" className={styles.phoneNavContent}>
               <FaBell />
               <br />
               通知
             </Link>
           </div>
-          <div className="col">
+          <div class="col">
             <Link href="" className={styles.phoneNavContent}>
               <FaUserAlt />
               <br />
               會員中心
             </Link>
           </div>
-          <div className="col">
+          <div class="col">
             <Link href="" className={styles.phoneNavContent}>
               <FaShop />
               <br />
               店舖管理
             </Link>
           </div>
-          <div className="col">
+          <div class="col">
             <Link href="" className={styles.phoneNavContent}>
               <FaCartShopping />
               <br />
--- next/pages/products/index.js
+++ next/pages/products/index.js
@@ -4,27 +4,28 @@ import { FaBorderAll } from 'react-icons/fa'
 import { IoReorderFour } from 'react-icons/io5'
 import ProductList from '@/components/products/product-list'
 import Breadcrumb from '@/components/products/breadcrumb'
-import Paginage from '@/components/common/pagination'
+// import Paginage from '@/components/common/paginage'
+import Pagination from '@/components/common/pagination'
 import BtnOutline from '@/components/products/btn-outline'
 import Link from 'next/link'
 import styles from '../../styles/products/products.module.scss'
 import Footer from '@/components/layout/footer/footer-front'
 import Navbar from '@/components/layout/navbar/navbar'
-import pImg from '@/public/images/product/p-index.jpg'
+import PhoneTabNav from '@/components/layout/navbar/phone-TabNav'
 
 export default function Products() {
   return (
     <>
       <Navbar />
-
       <Image
-        src={pImg}
+        src="/images/product/p-index.jpg"
         alt="product"
         width={1440}
         height={560}
         priority={true}
         className={`${styles.pIndexImg}`}
       />
+      <PhoneTabNav />
 
       <div
         className={`${styles.pTitle} z-2 position-absolute d-lg-grid d-none`}
@@ -49,40 +50,50 @@ export default function Products() {
             <IoReorderFour className="text-white h4 mb-0" />
           </div>
         </div>
-        <div className={`${styles.wrap}`}>
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
+        <div class="container px-0 py-2">
+          <div class="row row-cols-2 row-cols-lg-5 g-0 g-lg-3">
+            <div class="col">
+              <ProductList className="p-5" />
+            </div>
+            <div class="col">
+              <ProductList className="p-5" />
+            </div>
+            <div class="col">
+              <ProductList className="p-5" />
+            </div>
+            <div class="col">
+              <ProductList className="p-5" />
+            </div>
+            <div class="col">
+              <ProductList className="p-5" />
+            </div>
+            <div class="col">
+              <ProductList className="p-5" />
+            </div>
+            <div class="col">
+              <ProductList className="p-5" />
+            </div>
+          </div>
         </div>
-        <Paginage></Paginage>
-        <h4 className="text-white mb-2">猜你喜歡</h4>
-        <div className={`${styles.wrap}`}>
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
+        <Pagination />
+
+        <div>
+          <h4 className="text-white mx-2 ">猜你喜歡</h4>
+          <div class={`px-0 py-2 ${styles.guessLike}`}>
+            <ProductList className="" />
+            <ProductList className="" />
+            <ProductList className="" />
+            <ProductList className="" />
+            <ProductList className="" />
+          </div>
+
+          <Link
+            href=""
+            className={`d-block mt-2 text-end more h6 text-white ${styles.more}`}
+          >
+            看更多...
+          </Link>
         </div>
-        <Link href="">
-          <h6 className="text-end">看更多...</h6>
-        </Link>
       </div>
       <Footer />
     </>
