39a8254b45629be420566f9ef0baab4d0b7cc755
Wendy <wendy229880@gmail.com>
2024-03-04 00:22:22 +0800

    連線會員資料表取得對應商品的賣場名稱

---
+++ express/routes/cart.mjs
@@ -0,0 +1,35 @@
+import express from 'express'
+const router = express.Router()
+import db from '../configs/db.mjs'
+
+router.get('/shop-names', async (req, res) => {
+  try {
+    // 從client端傳來的賣場id參數們
+    const memberIds = req.query.memberIds
+
+    if (!memberIds) {
+      return res.status(400).json({ message: 'memberIds are required' })
+    }
+    // 建立相對應client端傳來的賣場id參數們的佔位字符串'?'
+    const memberIdsParams = memberIds
+      .split(',')
+      .map(() => '?')
+      .join(',')
+
+    const [rows] = await db.execute(
+      `SELECT id, shop_name FROM member WHERE id IN (${memberIdsParams})`,
+      memberIds.split(',')
+    )
+
+    const shopNames = rows.reduce((group, item) => {
+      group[item.id] = item.shop_name
+      return group
+    }, {})
+    res.json(shopNames)
+  } catch (error) {
+    console.error('伺服器連線失敗', error)
+    res.status(500).json({ message: '伺服器連線失敗' })
+  }
+})
+
+export default router
