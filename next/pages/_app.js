6a3479003a759803dfe6d87c4fb19412c733f0ab
Popomac <chiwen8697@gmail.com>
2024-03-04 23:57:46 +0800

    測試member & cart navbar

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -5,9 +5,12 @@ import '@/styles/globals.scss'
 
 import DefaultLayout from '@/components/layout/default-layout'
 //身分驗證
-import { AuthProvider } from '@/hooks/use-Auth';
+import { AuthProvider } from '@/context/AuthContext';
 import { getRedirectResult } from "firebase/auth";
 import { auth } from '@/utils/firebaseConfig';
+
+// 導入購物車Provider
+import { CartProvider } from '@/hooks/use-cart'
 
 export default function MyApp({ Component, pageProps }) {
   // 導入bootstrap的JS函式庫
@@ -24,7 +27,6 @@ export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    
     getRedirectResult(auth)
       .then((result) => {
         if (result) {
@@ -34,7 +36,6 @@ export default function MyApp({ Component, pageProps }) {
             headers: {
               'Content-Type': 'application/json',
             },
-            credentials: 'include',
             body: JSON.stringify({
               uid: gmember.uid,
               email: gmember.email,
@@ -61,7 +62,8 @@ export default function MyApp({ Component, pageProps }) {
   // 我把AuthProvider放在最外面 所有應用都能用
   return (
     <AuthProvider>
-      {getLayout(<Component {...pageProps} />)}
+    <CartProvider>{getLayout(<Component {...pageProps} />)}</CartProvider>
+      
     </AuthProvider>
   );
 }
