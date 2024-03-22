40217f4bbb36cdb59098caa7a313c16839d70098
Wendy <wendy229880@gmail.com>
2024-03-22 11:14:51 +0800

    新增loading動畫

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,18 +1,24 @@
 import { useEffect } from 'react'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
 // 樣式
 import '@/styles/globals.scss'
-import Swal from 'sweetalert2';
+import '@/styles/loader.scss'
+import Swal from 'sweetalert2'
+
+// 載入動畫context
+import { LoaderProvider } from '@/hooks/use-loader'
 import DefaultLayout from '@/components/layout/default-layout'
+// 自訂用載入動畫元件
+import { CatLoader, NoLoader } from '@/hooks/use-loader/components'
 //身分驗證
-import { AuthProvider } from '@/hooks/use-Auth';
-import { getRedirectResult } from "firebase/auth";
-import { auth } from '@/utils/firebaseConfig';
-import WithWebSocketProvider from '@/context/member/withloginWebsocket';
+import { AuthProvider } from '@/hooks/use-Auth'
+import { getRedirectResult } from 'firebase/auth'
+import { auth } from '@/utils/firebaseConfig'
+import WithWebSocketProvider from '@/context/member/withloginWebsocket'
 
 // 導入購物車Provider
 import { CartProvider } from '@/hooks/use-cart'
-import { ShippingProvider } from '@/hooks/use-shipping';
+import { ShippingProvider } from '@/hooks/use-shipping'
 
 export default function MyApp({ Component, pageProps }) {
   // 導入bootstrap的JS函式庫
@@ -26,20 +32,20 @@ export default function MyApp({ Component, pageProps }) {
     Component.getLayout || ((page) => <DefaultLayout>{page}</DefaultLayout>)
 
   //google api
-  const router = useRouter();
+  const router = useRouter()
 
   useEffect(() => {
     getRedirectResult(auth)
       .then((result) => {
         if (result) {
           Swal.fire({
-            position: "center",
-            icon: "success",
-            title: "登入成功！歡迎您！",
+            position: 'center',
+            icon: 'success',
+            title: '登入成功！歡迎您！',
             showConfirmButton: false,
-            timer: 1300
-          });
-          const gmember = result.user;
+            timer: 1300,
+          })
+          const gmember = result.user
           fetch('http://localhost:3005/api/member/google-login', {
             method: 'POST',
             headers: {
@@ -52,35 +58,34 @@ export default function MyApp({ Component, pageProps }) {
               photoURL: gmember.photoURL,
             }),
           })
-            .then(response => response.json())
-            .then(data => {
-              console.log('登入成功:', data);
-              router.replace('/');
+            .then((response) => response.json())
+            .then((data) => {
+              console.log('登入成功:', data)
+              router.replace('/')
             })
-            .catch(error => {
-              console.error('登入失敗:', error);
-            });
+            .catch((error) => {
+              console.error('登入失敗:', error)
+            })
         }
       })
       .catch((error) => {
-        console.error('登入錯誤:', error);
-        Swal.close();
-      });
-  }, [router]);
-
+        console.error('登入錯誤:', error)
+        Swal.close()
+      })
+  }, [router])
 
   // 我把AuthProvider放在最外面 所有應用都能用
   return (
-    <AuthProvider>
-      <CartProvider>
-        <ShippingProvider>
-          <WithWebSocketProvider>
-            {getLayout(<Component {...pageProps} />)}
-          </WithWebSocketProvider>
-        </ShippingProvider>
-      </CartProvider>
-    </AuthProvider>
-  );
+    <LoaderProvider close={3} CustomLoader={CatLoader} global={true}>
+      <AuthProvider>
+        <CartProvider>
+          <ShippingProvider>
+            <WithWebSocketProvider>
+              {getLayout(<Component {...pageProps} />)}
+            </WithWebSocketProvider>
+          </ShippingProvider>
+        </CartProvider>
+      </AuthProvider>
+    </LoaderProvider>
+  )
 }
-
-
